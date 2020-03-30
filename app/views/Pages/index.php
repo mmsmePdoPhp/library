8fd073be7a2f9258fcab0d0cad10a5f6b9900f72
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-03-30 19:59:44 +0430

    update card book for display book

--- app/views/Pages/index.php
+++ app/views/Pages/index.php
@@ -6,11 +6,10 @@
 
 
     <?php for ($i = 0; $i < 8; $i++) { ?>
-        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 mb-3 justify-content-between">
-            <div class="card px-1">
-            <div class="row">
+        <div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 mb-3 justify-content-between card-body card-book">
+            <div class="row border border-info">
                 <div class="card col-6 h-100 my-auto px-0" style="width: 10rem;">
-                    <img class="card-img-top img-fluid w-100" src="https://m.media-amazon.com/images/I/71tLAzeDwsL._AC_UY218_ML3_.jpg" alt="Card image cap">
+                    <img class="card-img-top img-fluid" src="https://m.media-amazon.com/images/I/71tLAzeDwsL._AC_UY218_ML3_.jpg" alt="Card image cap">
                     <!-- <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -22,18 +21,17 @@
 
                 <div class="card col-6 px-0" style="width: 10rem;">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">learning laravel 6.x</li>
-                        <li class="list-group-item"><i>by </i><strong>jenefer alma</strong></li>
-                        <li class="list-group-item"><i>published: 2020</i></li>
-                        <li class="list-group-item"><i>price:</i> 20$</li>
-                        <li class="list-group-item d-flex">
+                        <li class="list-group-item px-2">learning laravel 6.x</li>
+                        <li class="list-group-item px-2"><i>by </i><strong>jenefer alma</strong></li>
+                        <li class="list-group-item px-2"><i>published: 2020</i></li>
+                        <li class="list-group-item px-2"><i>price:</i> 20$</li>
+                        <li class="list-group-item px-2 d-flex">
                             <button class="btn btn-info btn-sm mr-auto">more</button>
                             <button class="btn btn-primary btn-sm ml-auto">download</button>
                         </li>
                     </ul>
                 </div>
 
-            </div>
             </div>
         </div>
 
